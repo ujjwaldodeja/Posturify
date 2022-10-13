47eb12c2224940a0972c2313fc5653790499b381
Danila <77702061+dkrgn@users.noreply.github.com>
2022-10-13 14:36:32 +0200

    #map initial implementation

--- app/src/main/java/com/example/posturfiy/MainActivity.java
+++ app/src/main/java/com/example/posturfiy/MainActivity.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,11 +22,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.posturfiy.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    private String test;
+    private static GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,5 +68,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public static void putMarker(double latitude, double longitude, String name) {
+        LatLng latLng = new LatLng(latitude, longitude);
+        map.addMarker(new MarkerOptions().position(latLng).title(name));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16.0f));
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
     }
 }
