937777b7cad2ac88a669e31d5c624f43dcf877a5
ujjwaldodeja <ujjwaldodeja@gmail.com>
2022-10-13 14:01:45 +0200

    Initial Commit

--- app/src/main/java/com/example/posturfiy/MainActivity.java
+++ app/src/main/java/com/example/posturfiy/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
